5bf11adeb012e0c782f904825cc3c2c70806a182
Andriy323 <108237550+Andriy323@users.noreply.github.com>
2023-02-14 10:55:35 +0200

    add loader

--- src/components/page/HomePage/Home.jsx
+++ src/components/page/HomePage/Home.jsx
@@ -4,16 +4,20 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useLocation } from 'react-router-dom';
 import RenderFilms from 'components/RenderFilms/RenderFilms';
+import Loader from 'components/shared/Loader/Loader';
 const Home = () => {
   const [state, setState] = useState({
     error: null,
     films: [],
+    loader: false,
   });
 
   const location = useLocation();
   useEffect(() => {
     const fetchTraidingFilms = async () => {
       try {
+        setState(prevState => ({ ...prevState, loader: true }));
+
         const { results } = await getTraidingFilms();
         setState(prevState => {
           return { ...prevState, films: [...prevState.films, ...results] };
@@ -22,13 +26,15 @@ const Home = () => {
         toast.error(
           ` An error occurred. Go to the main page or repeat the request.`
         );
+      } finally {
+        setState(prevState => ({ ...prevState, loader: false }));
       }
     };
     fetchTraidingFilms();
   }, []);
-
   return (
     <>
+      {state.loader && <Loader />}
       <ToastContainer />
       <RenderFilms items={state.films} location={location} />
     </>
--- src/components/page/MoviesPage/Movies.jsx
+++ src/components/page/MoviesPage/Movies.jsx
@@ -1,5 +1,6 @@
 import { useSearchParams, useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import Loader from 'components/shared/Loader/Loader';
 import FormSearch from 'components/formSearch/FormSearch';
 import RenderFilms from 'components/RenderFilms/RenderFilms';
 import { getFilmsSearch } from 'components/shared/shared';
@@ -7,6 +8,7 @@ import { getFilmsSearch } from 'components/shared/shared';
 const Movies = () => {
   const [items, setItems] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
+  const [loader, setLoader] = useState(false);
   const search = searchParams.get('search');
   const location = useLocation();
   useEffect(() => {
@@ -15,10 +17,13 @@ const Movies = () => {
     }
     const fetchSearch = async () => {
       try {
+        setLoader(prev => !prev);
         const { results } = await getFilmsSearch(search);
         setItems([...results]);
       } catch (error) {
         console.log(error);
+      } finally {
+        setLoader(prev => !prev);
       }
     };
     fetchSearch();
@@ -27,9 +32,10 @@ const Movies = () => {
   const onHangleSubmit = query => {
     setSearchParams({ search: query });
   };
-
+  console.log('loader Mov', loader);
   return (
     <div>
+      {loader && <Loader />}
       <FormSearch onHangleSubmit={onHangleSubmit} />
       <RenderFilms items={items} location={location} />
     </div>
